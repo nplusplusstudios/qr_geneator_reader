6568f1cd3854a3075b2b690139e062bb09f8be3a
mohamed nuzrath <nplusplusstudios@gmail.com>
2022-12-11 23:30:34 +0530

    creted the GUI MainScree

--- src/main/java/MyQr.java
+++ src/main/java/MyQr.java
@@ -15,6 +15,32 @@ import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 public class MyQr {
 
+        public static void createQRCode(String data){
+            /**/
+            // The data that the QR code will contain
+            //String data = "https://nplusplus.wordpress.com\nMohamed Nuzrath\nMawanella\nDeveloper";
+
+            // The path where the image will get saved
+            String path = "demo.png";
+            ///home/nuzrath/NetBeansProjects/qr_project_with_java_and_maven/src/main/java
+            // Encoding charset
+            String charset = "UTF-8";
+
+            Map<EncodeHintType, ErrorCorrectionLevel> hashMap
+                    = new HashMap<EncodeHintType,
+                                            ErrorCorrectionLevel>();
+
+            hashMap.put(EncodeHintType.ERROR_CORRECTION,
+                                    ErrorCorrectionLevel.L);
+            /**/
+            try{
+                createQR(data,path,charset,hashMap,200,200);
+            }catch(Exception er){
+                System.out.println(er.toString());
+            }
+            
+        }
+    
 	// Function to create the QR code
 	public static void createQR(String data, String path,
                                     String charset, Map hashMap,
